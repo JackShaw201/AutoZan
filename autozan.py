8270faa575bea96290ce8780db199e7d8fd696b2
JackShaw201 <xshaw201@gmail.com>
2025-01-26 19:48:08 +0800

    add click like-btn

--- autozan.py
+++ autozan.py
@@ -1,6 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
 
 def run(qq, loginmode):
     driver = webdriver.Chrome()
     driver.get("https://user.qzone.qq.com/" + qq  + "/infocenter")
+    time.sleep(10)
+    likebtn = driver.find_elements(By.CLASS_NAME, "qz_like_btn_v3")
+    for i in likebtn:
+        i.click()
